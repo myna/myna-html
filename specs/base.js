d17fd01b30ebf99532d151019debd29fc3714d7b
Wesley Moore <wes@wezm.net>
2013-01-16 12:03:30 +1100

    Handle reward responses that are type name != ok

--- specs/base.js
+++ specs/base.js
@@ -28,6 +28,41 @@ function fakeWorkingServer(options) {
   }
 }
 
+function fakeProblematicServer(options) {
+  options = options || {};
+
+  var tokenNum  = 1;
+  var choices   = options.choices || ["variant1", "variant2"];
+  var choiceNum = 0;
+
+  return function(params) {
+    if(/suggest/.test(params.url)) {
+      var token  = "token" + tokenNum;
+      var choice = choices[choiceNum];
+
+      tokenNum   = tokenNum + 1;
+      choiceNum  = (choiceNum + 1) % choices.length;
+
+      return new $.Deferred().done(params.success).resolve({
+        "typename" : "suggestion",
+        "token"    : token,
+        "choice"   : choice
+      }).promise();
+    } else if(/reward/.test(params.url)) {
+      return new $.Deferred().done(params.success).resolve({
+        "typename" : "problem",
+        "subtype"  : 400,
+        "messages" : [
+          {"typename": "malformedRequest"},
+          {"typename": "info", "message": "Reward token not present in cache"}
+        ]
+      }).promise();
+    } else {
+      throw [ "bad url", params.url ];
+    }
+  }
+}
+
 function fakeOfflineServer(options) {
   return function(params) {
     return new $.Deferred().done(params.success).promise();
